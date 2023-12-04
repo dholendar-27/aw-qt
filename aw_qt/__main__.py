e8ca8bbf41b5cf113c4463837da20be73d44ab9e
dholendar29 <146914063+dholendar29@users.noreply.github.com>
2023-12-04 15:46:23 +0530

    Implemented RA-91: Running only single instance

--- aw_qt/__main__.py
+++ aw_qt/__main__.py
@@ -1,3 +1,42 @@
 from aw_qt.main import main
+import os
+import sys
+import ctypes
 
-main()
+def check_lock_file(lock_file_path):
+    if os.path.exists(lock_file_path):
+        ctypes.windll.user32.MessageBoxW(0, 
+                                         "Sundial is already running.", 
+                                         "Sundial.exe", 
+                                         0x10)  # 0x10 is the code for an error icon
+        return True
+    return False
+
+def create_lock_file(lock_file_path):
+    try:
+        with open(lock_file_path, 'w') as lock_file:
+            lock_file.write("")
+    except Exception as e:
+        print(e)
+        return False
+    return True
+
+def delete_lock_file(lock_file_path):
+    try:
+        os.remove(lock_file_path)
+    except Exception as e:
+        print(e)
+
+
+lock_file_path = os.path.join(os.getenv('TEMP'), 'sundail.lock')
+
+if check_lock_file(lock_file_path):
+    sys.exit(0)
+
+if create_lock_file(lock_file_path):
+    try:
+        main()
+    finally:
+        delete_lock_file(lock_file_path)
+
+
